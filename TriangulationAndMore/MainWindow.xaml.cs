8ab5fdc5a1a804cd69bac43e3db3f49e7fdea5ae
Mikele <smeshmike@yandex,ru>
2021-04-30 14:37:42 +0300

    Base

---
+++ TriangulationAndMore/MainWindow.xaml.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static TriangulationAndMore.PointsProcessing;
+
+namespace TriangulationAndMore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private GeometryModel3D SurfaceModel;
+        private Model3DGroup MainModel3Dgroup = new Model3DGroup();
+        // The wireframe's model.
+        private GeometryModel3D VertexNormalsModel;
+        private GeometryModel3D WireframeModel;
+        private List<Triangle> triangle;
+        private List<PointF> points;
+        private int _pointsCount;
+
+        private PerspectiveCamera TheCamera;// The change in CameraPhi when you press the up and down arrows.
+        private const double CameraDPhi = 0.1;
+
+        // The change in CameraTheta when you press the left and right arrows.
+        private const double CameraDTheta = 0.1;
+
+        // The change in CameraR when you press + or -.
+        private const double CameraDR = 0.1;
+
+        private double CameraPhi = Math.PI / 6.0;  // 30 degrees
+        private double CameraTheta = Math.PI / 6.0;// 30 degrees
+        private double CameraR = 190.0;
+
+        const double xmin = 0;
+        const double xmax = 50;
+        const double dx = 1;
+        const double zmin = 0;
+        const double zmax = 50;
+        const double dz = 1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        // Create the scene.
+        // MainViewport is the Viewport3D defined
+        // in the XAML code that displays everything.
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            _pointsCount = Convert.ToInt32(pointsCount.Text);
+            TheCamera = new PerspectiveCamera();
+            TheCamera.FieldOfView = 60;
+            MainViewport.Camera = TheCamera;
+            PositionCamera();
+
+            // Define lights.
+            DefineLights();
+            PointsProcessing pp = new PointsProcessing();
+            triangle = pp.GenerateTriangles(xmin, xmax, zmin, zmax, _pointsCount);
+            points = pp.points;
+            // Create the model.
+            DefineModel(MainModel3Dgroup);
+
+            // Add the group of models to a ModelVisual3D.
+            ModelVisual3D model_visual = new ModelVisual3D();
+            model_visual.Content = MainModel3Dgroup;
+
+            // Display the main visual in the viewportt.
+            MainViewport.Children.Add(model_visual);
+        }
+
+        private void DefineModel(Model3DGroup model_group)
+        {
+            // Make a mesh to hold the surface.
+            MeshGeometry3D mesh = new MeshGeometry3D();
+
+
+            //for (double x = xmin; x <= xmax - dx; x += dx)
+            //{
+            //    for (double z = zmin; z <= zmax - dz; z += dx)
+            //    {
+            //        // Make points at the corners of the surface
+            //        // over (x, z) - (x + dx, z + dz).
+            //        Point3D p00 = new Point3D(x, 0, z);
+            //        Point3D p10 = new Point3D(x + dx, 0, z);
+            //        Point3D p01 = new Point3D(x, 0, z + dz);
+            //        Point3D p11 = new Point3D(x + dx, 0, z + dz);
+
+            //        // Add the triangles.
+            //        AddTriangle(mesh, p00, p01, p11);
+            //        AddTriangle(mesh, p00, p11, p10);
+            //    }
+            //}
+
+            for (int i = 0; i < triangle.Count; i++)
+            {
+                Point3D p1 = new Point3D(triangle[i].point1.X, 0, triangle[i].point1.Y);
+                Point3D p2 = new Point3D(triangle[i].point2.X, 0, triangle[i].point2.Y);
+                Point3D p3 = new Point3D(triangle[i].point3.X, 0, triangle[i].point3.Y);
+
+
+                // Add the triangles.
+                AddTriangle(mesh, p1, p2, p3);
+            }
+            
+
+            // Make the surface's material using a solid green brush.
+                DiffuseMaterial surface_material = new DiffuseMaterial(Brushes.Orange);
+
+            // Make the surface's model.
+            SurfaceModel = new GeometryModel3D(mesh, surface_material);
+
+            // Make the surface visible from both sides.
+            SurfaceModel.BackMaterial = surface_material;
+
+            // Add the model to the model groups.
+            model_group.Children.Add(SurfaceModel);
+
+            // Make a wireframe.
+            double thickness = 0.3;
+            double length = 5;
+
+            MeshGeometry3D wireframe = mesh.ToWireframe(thickness);
+            DiffuseMaterial wireframe_material = new DiffuseMaterial(Brushes.Black);
+            WireframeModel = new GeometryModel3D(wireframe, wireframe_material);
+            model_group.Children.Add(WireframeModel);
+
+            MeshGeometry3D vnormals = mesh.ToVertexNormals(length, thickness);
+            DiffuseMaterial vnormals_material = new DiffuseMaterial(Brushes.Green);
+            VertexNormalsModel = new GeometryModel3D(vnormals, vnormals_material);
+            model_group.Children.Add(VertexNormalsModel);
+        }
+
+        void Init()// Функция инцилизации компонентов
+        {
+
+        }
+
+        private void TriangulationButtonClick(object sender, RoutedEventArgs e)
+        {
+            MainModel3Dgroup.Children.Clear();
+            MainViewport.Children.Clear();
+            _pointsCount = Convert.ToInt32(pointsCount.Text);
+            MainViewport.Camera = TheCamera;
+            PositionCamera();
+
+            // Define lights.
+            DefineLights();
+            PointsProcessing pp = new PointsProcessing();
+            triangle = pp.GenerateTriangles(xmin, xmax, zmin, zmax, _pointsCount);
+            points = pp.points;
+            // Create the model.
+            DefineModel(MainModel3Dgroup);
+
+            // Add the group of models to a ModelVisual3D.
+            ModelVisual3D model_visual = new ModelVisual3D();
+            model_visual.Content = MainModel3Dgroup;
+
+            // Display the main visual in the viewportt.
+            MainViewport.Children.Add(model_visual);
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.W:
+                    CameraPhi += CameraDPhi;
+                    if (CameraPhi > Math.PI / 2.0) CameraPhi = Math.PI / 2.0;
+                    break;
+                case Key.S:
+                    CameraPhi -= CameraDPhi;
+                    if (CameraPhi < -Math.PI / 2.0) CameraPhi = -Math.PI / 2.0;
+                    break;
+                case Key.A:
+                    CameraTheta += CameraDTheta;
+                    break;
+                case Key.D:
+                    CameraTheta -= CameraDTheta;
+                    break;
+                case Key.Add:
+                case Key.OemPlus:
+                    CameraR -= CameraDR;
+                    if (CameraR < CameraDR) CameraR = CameraDR;
+                    break;
+                case Key.Subtract:
+                case Key.OemMinus:
+                    CameraR += CameraDR;
+                    break;
+            }
+
+            // Update the camera's position.
+            PositionCamera();
+        }
+
+        // Position the camera.
+        private void PositionCamera()
+        {
+            // Calculate the camera's position in Cartesian coordinates.
+            double y = CameraR * Math.Sin(CameraPhi);
+            double hyp = CameraR * Math.Cos(CameraPhi);
+            double x = hyp * Math.Cos(CameraTheta);
+            double z = hyp * Math.Sin(CameraTheta);
+            TheCamera.Position = new Point3D(x, y, z);
+
+            // Look toward the origin.
+            TheCamera.LookDirection = new Vector3D(-x, -y, -z);
+
+            // Set the Up direction.
+            TheCamera.UpDirection = new Vector3D(0, 1, 0);
+
+            // Console.WriteLine("Camera.Position: (" + x + ", " + y + ", " + z + ")");
+        }
+
+
+        private void AddTriangle(MeshGeometry3D mesh, Point3D point1, Point3D point2, Point3D point3)
+        {
+
+            // Get the points' indices.
+            int index1 = AddPoint(mesh.Positions, point1);
+            int index2 = AddPoint(mesh.Positions, point2);
+            int index3 = AddPoint(mesh.Positions, point3);
+
+            // Create the triangle.
+            mesh.TriangleIndices.Add(index1);
+            mesh.TriangleIndices.Add(index2);
+            mesh.TriangleIndices.Add(index3);
+        }
+
+        private int AddPoint(Point3DCollection points, Point3D point)
+        {
+            // If the point is in the point dictionary,
+            // return its saved index.
+            if (PointDictionary.ContainsKey(point))
+                return PointDictionary[point];
+
+            // We didn't find the point. Create it.
+            points.Add(point);
+            PointDictionary.Add(point, points.Count - 1);
+            return points.Count - 1;
+        }
+
+        // A dictionary to hold points for fast lookup.
+        private Dictionary<Point3D, int> PointDictionary =
+                new Dictionary<Point3D, int>();
+        private void DefineLights()
+        {
+            AmbientLight ambient_light = new AmbientLight(Colors.Gray);
+            DirectionalLight directional_light =
+                    new DirectionalLight(Colors.Gray, new Vector3D(-1.0, -3.0, -2.0));
+            MainModel3Dgroup.Children.Add(ambient_light);
+            MainModel3Dgroup.Children.Add(directional_light);
+        }
+    }
+}
---
+++ TriangulationAndMore/PointsProcessing.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Media;
+using Color = System.Windows.Media.Color;
+
+namespace TriangulationAndMore
+{
+    public class PointsProcessing
+    {
+        public class Triangle
+        {
+            public PointF point1;
+            public PointF point2;
+            public PointF point3;
+
+            public Triangle(PointF p1, PointF p2, PointF p3)
+            {
+                point1 = p1;
+                point2 = p2;
+                point3 = p3;
+            }
+        }
+
+        private double MaxX { get; set; }
+        private double MaxY { get; set; }
+        private IEnumerable<Triangle> border;
+
+        public IEnumerable<Point> GeneratePoints(int amount, double maxX, double maxY)
+        {
+            MaxX = maxX;
+            MaxY = maxY;
+
+            // TODO make more beautiful
+            var point0 = new Point(0, 0);
+            var point1 = new Point(0, MaxY);
+            var point2 = new Point(MaxX, MaxY);
+            var point3 = new Point(MaxX, 0);
+            var points = new List<Point>() { point0, point1, point2, point3 };
+            var tri1 = new Triangle(point0, point1, point2);
+            var tri2 = new Triangle(point0, point2, point3);
+            border = new List<Triangle>() { tri1, tri2 };
+
+            var random = new Random();
+            for (int i = 0; i < amount - 4; i++)
+            {
+                var pointX = random.NextDouble() * MaxX;
+                var pointY = random.NextDouble() * MaxY;
+                points.Add(new Point(pointX, pointY));
+            }
+
+            return points;
+        }
+
+
+        public List<Triangle> triangles;
+        public List<PointF> points;
+
+        public List<Triangle> GenerateTriangles(double xMin, double xMax, double yMin, double yMax, int count)
+        {
+            GenerateDots(xMin, xMax, yMin, yMax, 8, 8);
+            FindTriangles();
+            return triangles;
+        }
+
+        void GenerateDots(double xMin, double xMax, double yMin, double yMax, int width, int height)
+        {
+
+            points = new List<PointF>();
+            PointF tmpPointF = PointF.Empty;
+            float iShift = 0.2f;
+            float jShift =0.2f;
+            for (int i = -width/2; i < width/2; ++i)
+            {
+                for (int j = -height/2; j < height/2; ++j)
+                {
+                    int iSign = i % 2 == 0 ? 1 : -1;
+                    int jSign = j % 2 == 0 ? 1 : -1;
+                    tmpPointF.X = Convert.ToSingle((i + 1) * 10) + iShift*iSign*jSign + jShift * iSign * jSign;
+                    tmpPointF.Y = Convert.ToSingle((j + 1) * 10) + jShift * iSign * jSign +iShift* iSign*jSign;
+                    points.Add(tmpPointF);
+                }
+            }
+        }
+
+        void GenerateRandomDots(double xMin, double xMax, double yMin, double yMax, int count)
+        {
+            var xCoef = Math.Abs(xMin) + Math.Abs(xMax);
+            var yCoef = Math.Abs(yMin) + Math.Abs(yMax);
+
+            points = new List<PointF>();
+            PointF tmpPointF = PointF.Empty;
+
+            for (int i = 0; i < count; i++)
+            {
+
+                tmpPointF.X = (float)(new Random().NextDouble() * xCoef - xCoef / 2);
+                tmpPointF.Y = (float)(new Random().NextDouble() * yCoef - yCoef / 2);
+                points.Add(tmpPointF);
+            }
+        }
+
+        void FindTriangles()
+        {
+            triangles = new List<Triangle>();
+            PointF point1, point2, point3 = PointF.Empty;
+
+            for (int i = 0; i < points.Count-2; i++)
+            {
+                point1 = points[i];
+                for (int j = i+1; j < points.Count-1; j++)
+                {
+                    bool add;
+                    point2 = points[j];
+                    for (int k = j+1; k < points.Count; k++)
+                    {
+                        PointF c;
+                        float r;
+                        FindCircle(points[i], points[j], points[k],out c, out r);
+                        point3 = points[k];
+                        add = true;
+
+                        for (int l = 0; l < points.Count; l++)
+                        {
+                            if (l != k && l != j && l != i)
+                            {
+                                var x = points[l].X;
+                                var y = points[l].Y;
+
+                                if (((x - c.X) * (x - c.X) + (y - c.Y) * (y - c.Y) < r * r))
+                                {
+                                    add = false;
+                                    break;
+                                }
+                            }
+                        }
+                        if (add)
+                            triangles.Add(new Triangle(point1, point2, point3));
+                    }
+                }
+            }
+        }
+
+
+
+        // Find a circle through the three points.
+        private void FindCircle(PointF a, PointF b, PointF c, out PointF center, out float radius)
+        {
+            // Get the perpendicular bisector of (x1, y1) and (x2, y2).
+            var x1 = (b.X + a.X) / 2;
+            var y1 = (b.Y + a.Y) / 2;
+            var dy1 = b.X - a.X;
+            var dx1 = -(b.Y - a.Y);
+
+            // Get the perpendicular bisector of (x2, y2) and (x3, y3).
+            var x2 = (c.X + b.X) / 2;
+            var y2 = (c.Y + b.Y) / 2;
+            var dy2 = c.X - b.X;
+            var dx2 = -(c.Y - b.Y);
+
+            // See where the lines intersect.
+            FindIntersection(new PointF(x1, y1), new PointF(x1 + dx1, y1 + dy1), new PointF(x2, y2), new PointF(x2 + dx2, y2 + dy2), out var linesIntersect,
+                    out var intersection);
+            if (!linesIntersect)
+            {
+                center = new PointF(0, 0);
+                radius = 0;
+            }
+            else
+            {
+                center = intersection;
+                var dx = center.X - a.X;
+                var dy = center.Y - a.Y;
+                radius = (float) Math.Sqrt(dx * dx + dy * dy);
+            }
+        }
+
+        private void FindIntersection(PointF p1, PointF p2, PointF p3, PointF p4, out bool linesIntersect, out PointF intersection)
+        {
+            // Get the segments' parameters.
+            var dx12 = p2.X - p1.X;
+            var dy12 = p2.Y - p1.Y;
+            var dx34 = p4.X - p3.X;
+            var dy34 = p4.Y - p3.Y;
+
+            // Solve for t1 and t2
+            var denominator = (dy12 * dx34 - dx12 * dy34);
+
+            var t1 = ((p1.X - p3.X) * dy34 + (p3.Y - p1.Y) * dx34) / denominator;
+            if (float.IsInfinity(t1))
+            {
+                // The lines are parallel (or close enough to it).
+                linesIntersect = false;
+                intersection = new PointF(float.NaN, float.NaN);
+                return;
+            }
+
+            linesIntersect = true;
+            // Find the points of intersection.
+            intersection = new PointF(p1.X + dx12 * t1, p1.Y + dy12 * t1);
+        }
+
+
+
+    }
+}
